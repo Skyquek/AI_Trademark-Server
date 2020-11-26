c081ed8ba28d0446ade9aa02f49eb2b8000c87b3
KwongTN <tungnan5636@gmail.com>
2020-11-27 00:12:49 +0800

    [Angular] Search page wireframe

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 import { AppComponent } from './app.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SearchPageComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, SearchPageComponent],
 })
-export class AppModule { }
+export class AppModule {}
