c00b8403319d09cbfb2661019ab836f47aba7ea9
KwongTN <tungnan5636@gmail.com>
2020-11-27 22:10:20 +0800

    [Angular] Added route support

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+// Material Design Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+const routes: Routes = [
+  { path: '', component: SearchPageComponent },
+];
+
 @NgModule({
   declarations: [AppComponent, SearchPageComponent],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
+    MatToolbarModule,
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
     BrowserAnimationsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent, SearchPageComponent],
