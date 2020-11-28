114d36c2008e0d19e4626de58a4c204e02443a04
KwongTN <tungnan5636@gmail.com>
2020-11-29 01:44:24 +0800

    [Angular] Passed search form data to backend

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 // Material Design Modules
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchResultElementComponent } from './search-page/search-result-element/search-result-element.component';
 
-const routes: Routes = [
-  { path: '', component: SearchPageComponent },
-];
+const routes: Routes = [{ path: '', component: SearchPageComponent }];
 
 @NgModule({
-  declarations: [AppComponent, SearchPageComponent],
+  declarations: [
+    AppComponent,
+    SearchPageComponent,
+    SearchResultElementComponent,
+  ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
@@ -28,8 +34,11 @@ const routes: Routes = [
     MatFormFieldModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent, SearchPageComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- public/src/app/search-page/search-page.component.ts
+++ public/src/app/search-page/search-page.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
+
+const backend: String = 'http://localhost:3000';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
-  styleUrls: ['./search-page.component.scss']
+  styleUrls: ['./search-page.component.scss'],
 })
 export class SearchPageComponent implements OnInit {
+  searchError: boolean = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  onSearch(form: NgForm) {
+    if (form.value.searchString == '') {
+      this.searchError = true;
+      return;
+    } else {
+      this.http
+        .get(backend + '/db/query', {
+          params: new HttpParams().set('string', form.value.searchString),
+        })
+        .subscribe((res) => {
+          console.log(res);
+        });
+    }
   }
 
+  ngOnInit(): void {}
 }
