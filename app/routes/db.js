1dbbd683d6330309bf7dad1f5542a9efbdaff4c8
KwongTN <tungnan5636@gmail.com>
2020-11-28 16:03:09 +0800

    [Express] Add logic to handle empty search string

--- app/routes/db.js
+++ app/routes/db.js
@@ -17,56 +17,59 @@ try {
 
 /* Get matching trademark names */
 router.get('/query', async (req, res, next) => {
-
-  if (req.query.string.indexOf('*') >= 0) {
-    // If search query contains a wildcard symbol, we take that as user defined and do not do our own processing.
-
-    var sanitizedString = req.query.string.replace(/\*/g, '%');
-    console.log(sanitizedString);
-    dbConn.query("SELECT * FROM tbl_trademark WHERE brand LIKE ?",
-      [sanitizedString], (err, result) => {
-        if (err) throw err;
-        res.send(result);
-      });
-
+  if (!req.query['string']) {
+    res.send('No parameters passed.');
 
   } else {
-    console.log("No Wildcard");
-    /**
-     * Get results based on starting words
-     * @param {string} queryString String to search
-     */
-    function headMatch(queryString) {
-      return new Promise((resolve, reject) => {
-        dbConn.query("SELECT * FROM tbl_trademark WHERE UPPER(brand) LIKE ?",
-          [queryString.toUpperCase() + '%'], (err, result) => {
-            if (err) reject(err);
-            resolve(result);
-          });
+    if (req.query.string.indexOf('*') >= 0) {
+      // If search query contains a wildcard symbol, we take that as user defined and do not do our own processing.
+
+      var sanitizedString = req.query.string.replace(/\*/g, '%');
+      console.log(sanitizedString);
+      dbConn.query("SELECT * FROM tbl_trademark WHERE brand LIKE ?",
+        [sanitizedString], (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        });
+
+    } else {
+      /**
+       * Get results based on starting words
+       * @param {string} queryString String to search
+       */
+      function headMatch(queryString) {
+        return new Promise((resolve, reject) => {
+          dbConn.query("SELECT * FROM tbl_trademark WHERE UPPER(brand) LIKE ?",
+            [queryString.toUpperCase() + '%'], (err, result) => {
+              if (err) reject(err);
+              resolve(result);
+            });
+        });
+      }
+
+      /**
+      * 
+      * @param {string} queryString String to search
+      */
+      function otherMatch(queryString) {
+        return new Promise((resolve, reject) => {
+          dbConn.query("SELECT * FROM tbl_trademark WHERE UPPER(brand) LIKE ? AND number NOT IN (SELECT number FROM tbl_trademark WHERE UPPER(brand) LIKE ?)",
+            ['%' + queryString.toUpperCase() + '%', queryString.toUpperCase() + '%'],
+            (err, result) => {
+              if (err) reject(err);
+              resolve(result);
+            });
+
+        })
+      }
+
+      Promise.all([headMatch(req.query.string), otherMatch(req.query.string)]).then((values) => {
+        return values[0].concat(values[1]);
+      }).then((arrValues) => {
+        res.send(arrValues);
       });
+
     }
-
-    /**
-    * 
-    * @param {string} queryString String to search
-    */
-    function otherMatch(queryString) {
-      return new Promise((resolve, reject) => {
-        dbConn.query("SELECT * FROM tbl_trademark WHERE UPPER(brand) LIKE ? AND number NOT IN (SELECT number FROM tbl_trademark WHERE UPPER(brand) LIKE ?)",
-          ['%' + queryString.toUpperCase() + '%', queryString.toUpperCase() + '%'],
-          (err, result) => {
-            if (err) reject(err);
-            resolve(result);
-          });
-
-      })
-    }
-
-    Promise.all([headMatch(req.query.string), otherMatch(req.query.string)]).then((values) => {
-      return values[0].concat(values[1]);
-    }).then((arrValues) => {
-      res.send(arrValues);
-    });
 
   }
 });
