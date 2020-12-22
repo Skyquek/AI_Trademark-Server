27bd3b8769984b28ad41d0624a791f5c55170a56
KwongTN <tungnan5636@gmail.com>
2020-12-22 16:39:22 +0800

    [Ang] Temporary adding in barebones searchResultElementComponent

---
+++ public/src/app/search-page/search-result-element/search-result-element.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SearchResultElementComponent } from './search-result-element.component';
+
+describe('SearchResultElementComponent', () => {
+  let component: SearchResultElementComponent;
+  let fixture: ComponentFixture<SearchResultElementComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SearchResultElementComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SearchResultElementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ public/src/app/search-page/search-result-element/search-result-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-result-element',
+  templateUrl: './search-result-element.component.html',
+  styleUrls: ['./search-result-element.component.scss']
+})
+export class SearchResultElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
